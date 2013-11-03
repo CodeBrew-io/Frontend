b7bad2a28dcd196d3cda9d2f4251425aae5ef5c0
Guillaume Massé <masgui@gmail.com>
2013-11-03 14:11:58 -0500

    dist build

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,6 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9002,
-        // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
       livereload: {
@@ -117,44 +116,6 @@ module.exports = function (grunt) {
       },
       server: '.tmp'
     },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
-      ]
-    },
-    coffee: {
-      options: {
-        sourceMap: true,
-        sourceRoot: ''
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/scripts',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '{,*/}*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
-      }
-    },
-    // not used since Uglify task does concat,
-    // but still available if needed
-    /*concat: {
-      dist: {}
-    },*/
     rev: {
       dist: {
         files: {
@@ -174,8 +135,8 @@ module.exports = function (grunt) {
       }
     },
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      html: ['<%= yeoman.dist %>/{,*/}*.html'],
       options: {
         dirs: ['<%= yeoman.dist %>']
       }
@@ -199,19 +160,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>/images'
         }]
       }
-    },
-    cssmin: {
-      // By default, your `index.html` <!-- Usemin Block --> will take care of
-      // minification. This option is pre-configured if you do not wish to use
-      // Usemin blocks.
-      // dist: {
-      //   files: {
-      //     '<%= yeoman.dist %>/styles/main.css': [
-      //       '.tmp/styles/{,*/}*.css',
-      //       '<%= yeoman.app %>/styles/{,*/}*.css'
-      //     ]
-      //   }
-      // }
     },
     htmlmin: {
       dist: {
@@ -244,8 +192,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
-            'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
           ]
@@ -256,6 +202,11 @@ module.exports = function (grunt) {
           src: [
             'generated/*'
           ]
+        }, {
+          expand: true, 
+          cwd: '<%= yeoman.app %>/bower_components/font-awesome/fonts/', 
+          src: ['**'], 
+          dest: '<%= yeoman.dist %>/fonts/'
         }]
       },
       styles: {
@@ -267,7 +218,6 @@ module.exports = function (grunt) {
     },
     concurrent: {
       server: [
-        'coffee:dist',
         'less:dev',
         'copy:styles'
       ],
@@ -277,7 +227,6 @@ module.exports = function (grunt) {
         'copy:styles'
       ],
       dist: [
-        'coffee',
         'less:dist',
         'copy:styles',
         'imagemin',
@@ -359,14 +308,12 @@ module.exports = function (grunt) {
     'concat',
     'copy:dist',
     'ngmin',
-    'cssmin',
     'uglify',
     'rev',
     'usemin'
   ]);
 
   grunt.registerTask('default', [
-    'jshint',
     'test',
     'build'
   ]);
