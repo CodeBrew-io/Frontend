364f4bc1025cbe2220d9c310d88e8b82dba15c7e
Guillaume Massé <masgui@gmail.com>
2013-11-07 14:50:30 -0500

    simplify rest interface track by id

--- app/scripts/filters/restSnippet.js
+++
@@ -1,9 +0,0 @@
-app.filter('snippetCode',function(){
-	return function(inputs){
-		var filtered = [];
-    	angular.forEach(inputs, function(input) {
-    		filtered.push(input.snippet["code.origin"]);
-    	});
-		return filtered;
-	}
-});
