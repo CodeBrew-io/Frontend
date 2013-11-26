fac77da4b270afd280ea394f8a26e4ee2a2bb00d
Raouf Merouche <ashmed@gmail.com>
2013-11-26 15:30:27 -0500

    Add front end support for distinct snippet query

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -31,7 +31,7 @@ app.controller('header', function header($scope, $rootScope, user, snippets, sca
 			$scope.snippets = [];
 			$scope.all = [];
 		} else {
-			snippets.query({terms: term}, function(data){
+			snippets.queryDistinct({terms: term}, function(data){
 				$scope.snippets = data;
 				scaladoc.query(term).then(function(data){
 					$scope.docs = data;
--- app/scripts/services/snippets.js
+++ app/scripts/services/snippets.js
@@ -1,6 +1,7 @@
 app.factory('snippets', function($resource) {
 	return $resource('/snippets/',{},{
 		"queryUser": { method: 'GET', url: '/snippets/queryUser', isArray: true },
+		"queryDistinct": { method: 'GET', url: '/snippets/queryDistinct', isArray: true },
 		"query": { method: 'GET', isArray: true },
 		"save": { method: 'POST' },
 		"delete" : {
