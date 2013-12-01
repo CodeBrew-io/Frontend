ef62375ddd92f938ca11ef85b301642209930876
manbear <eric.comte.marois@gmail.com>
2013-11-30 21:07:40 -0500

    Changed how the bubbles are placed in the page, used jQuery

--- app/scripts/controllers/bubble.js
+++ app/scripts/controllers/bubble.js
@@ -1,11 +1,76 @@
 app.controller('bubble', function code($scope) {
-	$scope.currentBubbleIndex = 0;
-	$scope.show = false;
+	$scope.currentBubbleIndex = -1;
+	$scope.show = true;
+
+	// This object contains the tutorialText string for the tutorial's bubble.
+	$scope.tutorialText = {
+		previous: 'previous',
+		skip: 'skip',
+		next: 'next',
+		bubbleInfoList: [
+			{selector: '.code', message: "You can get the anwser to a quick question by writing Scala code into this editor"}
+			,{selector: '.insight', message: "The code is evaluated and displayed in the insight view"}
+			,{selector: '.console', message: "The output console can be open by clicking on the bottom right icon of the screen"}
+			,{selector: '.search-area', message: "You can use the search bar to find Scala documentation or to look for snippet code examples"}
+			//,{selector: '.icon', message: "You can use the icons to enter the fullscreen mode or to hide the insight view"}
+		]
+	};
+
+	// contains the model of the text rendered in the bubble.
+	$scope.bubbleText = '';
+
+	// needed for the changes that occurs on it (mostly the position changes)
+	$scope.bubbleJqueryElement = null; 
+
+	// FOR TESTS
+	$scope.bubbleLeft = '';
+	$scope.bubbleTop = '';
+
+	// will get the informations needed for the bubble
+	function _setBubbleData(index) {
+		_ensureBubbleElement();
+
+		var bubbleInfoList = $scope.tutorialText.bubbleInfoList;
+		var maxSize = bubbleInfoList.length;
+
+		if (index > -1 && index < maxSize) {
+			var bubbleInfo = bubbleInfoList[index];
+
+			// gets the target element's position
+			var element = $(bubbleInfo.selector);
+			if (element !== null && element !== undefined) {
+				//bubbleData.position = element.offset();
+				var elemOffset = element.offset();
+
+				if (elemOffset !== null && elemOffset !== undefined) {
+
+					// assign the new changes on the bubble's model
+					if ($scope.bubbleJqueryElement !== null 
+						&& $scope.bubbleJqueryElement !== undefined) 
+					{
+						$scope.bubbleText = bubbleInfo.message;
+						$scope.bubbleJqueryElement.offset(elemOffset);
+
+						$scope.bubbleLeft = elemOffset.left;
+						$scope.bubbleTop = elemOffset.top;
+					}
+				}
+			}
+		}
+	}
+
+	// Will ensure that the Bubble's element has been fetched by jQuery.
+	function _ensureBubbleElement() {
+		if ($scope.bubbleJqueryElement === null || $scope.bubbleJqueryElement === undefined) {
+			$scope.bubbleJqueryElement = $('.bubble');
+		}
+	}
 
 	// set the previous bubble as current index
 	$scope.previous = function() {
 		if ($scope.currentBubbleIndex > 0) {
 			$scope.currentBubbleIndex -= 1;
+			_setBubbleData($scope.currentBubbleIndex);
 		}
 	};
 
@@ -17,8 +82,10 @@ app.controller('bubble', function code($scope) {
 
 	// set the next bubble as current index
 	$scope.next = function() {
-		if ($scope.currentBubbleIndex < $scope.tutorialText.messages.length -1) {
+		if ($scope.currentBubbleIndex < $scope.tutorialText.bubbleInfoList.length -1) {
 			$scope.currentBubbleIndex += 1;
+			_setBubbleData($scope.currentBubbleIndex);
+
 		} else {
 			$scope.show = false;
 		}
@@ -31,12 +98,12 @@ app.controller('bubble', function code($scope) {
 	};
 
 	$scope.last = function() {
-		if ($scope.currentBubbleIndex == $scope.tutorialText.messages.length -1 ) {
+		if ($scope.currentBubbleIndex == $scope.tutorialText.bubbleInfoList.length -1 ) {
 			return "disabled";	
 		}
 	};
 
-
+	/*
 	$scope.getBubbleClass = function() {
 		var resultClassStr = "triangle-border ";
 		if ($scope.currentBubbleIndex == 0) {
@@ -60,20 +127,10 @@ app.controller('bubble', function code($scope) {
 			resultClassStr += "output";
 		}
 		return resultClassStr;
-	};
+	};*/
 
-	// This object contains the tutorialText string for the tutorial's bubble.
-	$scope.tutorialText = {
-		previous: 'previous',
-		skip: 'skip',
-		next: 'next',
-		messages: [
-			"You can get the anwser to a quick question by writing Scala code into this editor"
-			,"The code is evaluated and displayed in the insight view"
-			,"You can loggin here and get or save snippets of Scala code"
-			,"You can use the search bar to find Scala documentation or to look for snippet code examples"
-			,"You can use the icons to enter the fullscreen mode or to hide the insight view"
-			,"The output console can be open by clicking on the bottom right icon of the screen"
-		]
-	};
+	/*/ assign the first tutorial bubble
+	$(document).ready(function() {
+		_setBubbleData(0);
+	});*/
 });
